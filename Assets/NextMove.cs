8d293513d1a39b8e24f775f70002d93c29d4a438
despair1 <eveguide0@gmail.com>
2017-04-19 20:19:08 +0500

    begin

---
+++ Assets/NextMove.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NextMove : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        Debug.Log("wtf&&&");
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnClick ()
+    {
+        Debug.Log(" Clicing ");
+    }
+}
