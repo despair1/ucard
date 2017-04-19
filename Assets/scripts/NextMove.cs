5f13ee1c047ad19493d51dd0a4ab4aefad8c60b8
despair1 <eveguide0@gmail.com>
2017-04-19 22:35:08 +0500

    load sprite by name

--- Assets/scripts/NextMove.cs
+++ Assets/scripts/NextMove.cs
@@ -1,9 +1,10 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class NextMove : MonoBehaviour {
-
+    public Button next;
 	// Use this for initialization
 	void Start () {
 
---
+++ Assets/scripts/hand.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hand : MonoBehaviour {
+    Sprite s1;
+    SpriteRenderer sr1;
+	// Use this for initialization
+	void Start () {
+        s1 = Resources.Load<Sprite>("cards/boss_1");
+        //s1 = Resources.Load<Sprite>("boss_1");
+        sr1 = gameObject.AddComponent<SpriteRenderer>();
+        sr1.sprite = s1;
+        //Instantiate(s1);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
