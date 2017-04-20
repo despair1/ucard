2e49c11b990d5b6d77fd10551866a4d887c44643
despair1 <eveguide0@gmail.com>
2017-04-20 20:52:30 +0500

    add card in hand by click next move
    Signed-off-by: despair1 <eveguide0@gmail.com>

--- Assets/scripts/hand.cs
+++ Assets/scripts/hand.cs
@@ -5,14 +5,50 @@ using UnityEngine;
 public class hand : MonoBehaviour {
     Sprite s1;
     SpriteRenderer sr1;
+    GameObject card;
+    class position
+    {
+        public float begin_x;
+        public float begin_y;
+        public float offset;
+        public int card_count;
+
+
+        public position(double x,double y,double ofst)
+        {
+            begin_x = (float)x ;
+            begin_y = (float)y;
+            offset = (float)ofst;
+            card_count = 0;
+        }
+    }
+    position p1=new position(-4,-3.5,0.7);
 	// Use this for initialization
 	void Start () {
-        s1 = Resources.Load<Sprite>("cards/boss_1");
+        /*s1 = Resources.Load<Sprite>("cards/boss_1");
         //s1 = Resources.Load<Sprite>("boss_1");
         sr1 = gameObject.AddComponent<SpriteRenderer>();
         sr1.sprite = s1;
-        //Instantiate(s1);
+        sr1.transform.position = new Vector3(-2, -4, 0);
+        //Instantiate(s1);*/
 	}
+
+    public void OnNextMove()
+    {
+        if (p1.card_count<9)
+        {
+            card = new GameObject("card");
+            s1 = Resources.Load<Sprite>("cards/boss_1");
+            //s1 = Resources.Load<Sprite>("boss_1");
+            sr1 = card.AddComponent<SpriteRenderer>();
+            sr1.sprite = s1;
+            sr1.transform.position = new Vector3(p1.begin_x+p1.card_count*p1.offset,
+                p1.begin_y, 0);
+            p1.card_count++;
+
+        }
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
